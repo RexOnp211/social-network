f204ac35b8e23619989107933cdbb6f04918e69f
jvillem <jurivallem@gmail.com>
2024-09-15 17:31:19 +0300

    added docker compose

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
 you can run the backend server by running the main.go file in backend/src/api
 to run the frontend server you can run "npm run dev" in the frontend dir
+
+to build docker run "docker compose build" and then "docker compose up" to run it
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,15 +1,15 @@
 import TopBar from "@/components/topbar";
 import SideBar from "@/components/sidebar";
+import FetchFromBackend from "@/lib/fetch";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:8080/");
-  const posts = await res.json();
+  const res = await FetchFromBackend("/");
   return (
     <>
       <TopBar />
       <SideBar />
       <h1>Home Page </h1>
-      <p>{posts.message}</p>
+      <p>{res.message}</p>
     </>
   );
 }
