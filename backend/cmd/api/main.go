d4514a56c0a6c942dcaa19f7dc1ecdbed9387aeb
jvillem <jurivallem@gmail.com>
2024-09-19 18:25:39 +0300

    fixed filepath error

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,39 +4,57 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 	"social-network/internal/api"
 	"social-network/internal/api/handlers"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "/backend/pkg/db/database.db")
-	if err != nil {
-		fmt.Println(err)
+
+	// Ensure the database file exists
+	if _, err := os.Stat("../../pkg/db/database.db"); os.IsNotExist(err) {
+		fmt.Println("Database file does not exist:", err)
+		return
 	}
+
+	// Correct database path
+	db, err := sql.Open("sqlite3", "../../pkg/db/database.db")
+	if err != nil {
+		fmt.Println("DB Open Error:", err)
+		return
+	}
+
 	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Driver Error:", err)
+		return
 	}
+
+	// Correct migration path
 	m, err := migrate.NewWithDatabaseInstance(
-		"file:///migrations",
-		"sqlite3", driver)
-	m.Up() // or m.Steps(2) if you want to explicitly set the number of migrations to run
+		"file://../../pkg/db/migrations/sqlite", "sqlite3", driver)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Migration Instance Error:", err)
+		return
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		fmt.Println("Migration Up Error:", err)
+		return
 	}
 
 	r := &api.Router{}
-
 	r.AddRoute("GET", "/posts", http.HandlerFunc(handlers.HandlePosts))
 	r.AddRoute("GET", "/", http.HandlerFunc(handlers.HomeHandler))
 
-	fmt.Println("starting go server")
+	fmt.Println("Starting Go server")
 	err = http.ListenAndServe(":8080", r)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Server Error:", err)
 	}
 }
