54bf079ed088aca312db2fff6c3b0af83e5445db
Magnus Markus Lutter <magnuslutter@gmail.com>
2024-09-19 17:43:58 +0300

    first migration

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"social-network/internal/api"
 	"social-network/internal/api/handlers"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "/backend/pkg/db/database.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file:///migrations",
+		"sqlite3", driver)
+	m.Up() // or m.Steps(2) if you want to explicitly set the number of migrations to run
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	r := &api.Router{}
 
 	r.AddRoute("GET", "/posts", http.HandlerFunc(handlers.HandlePosts))
 	r.AddRoute("GET", "/", http.HandlerFunc(handlers.HomeHandler))
 
 	fmt.Println("starting go server")
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", r)
 	if err != nil {
 		fmt.Println(err)
 	}
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,12 @@
 module social-network
 
 go 1.22.5
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.18.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.23 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
