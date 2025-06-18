a10fac0ce87732b2290cbdc569424d47fc66d763
RexOnp211 <illuminate.kmkz@outlook.com>
2025-06-18 22:52:28 +0300

    for render

--- frontend/src/lib/fetch.js
+++ frontend/src/lib/fetch.js
@@ -1,5 +1,5 @@
 export default async function FetchFromBackend(endPoint, options) {
-  const url = process.env.API_URL || "http://localhost:8080";
+  const url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
   const res = await fetch(`${url}${endPoint}`, options);
   return res;
 }
