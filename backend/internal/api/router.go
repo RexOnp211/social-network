dc4c529e1f95427680a05d10827cd38598e5e94b
RexOnp211 <illuminate.kmkz@outlook.com>
2025-06-21 22:25:37 +0300

    for render

--- backend/internal/api/router.go
+++ backend/internal/api/router.go
@@ -60,7 +60,7 @@ func corsHandler(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
 		if r.Method == http.MethodOptions {
